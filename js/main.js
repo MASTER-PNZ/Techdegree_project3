4ad4e2b3401f07badbec7825495ecbbad6a3d2fa
MASTER-PNZ <paulkeesling@gmail.com>
2018-04-11 11:58:48 -0400

    created function and changed variable types

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 // Variables to get html form elements
 
-
+let nameField = $('#name');
+let otherJob = $('#other-title');
 
 // Setting focus to first form field on pageLoad
 
-
+nameField.focus();
 
 // Hide the Other Job Role Html option until user clicks other
 // add user input to a text field that already exists in HTML file
 
-
+function jobTitle() {
+  otherJob.hide();
+  if($('#title').val("other")){
+    otherJob.show();
+  }
+}
+jobTitle();
 // function that hides the color option select menu until design option is chosen
 // and only displays T-shirt color options to matched design options
 
