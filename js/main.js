ae5c2c194c064045bd5668d31c729ef36a7ad402
MASTER-PNZ <paulkeesling@gmail.com>
2018-04-11 11:13:02 -0400

    Added comments and JS file
    Separate JS file for validation elements

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Variables to get html form elements
+
+
+
+// Setting focus to first form field on pageLoad
+
+
+
+// Hide the Other Job Role Html option until user clicks other
+// add user input to a text field that already exists in HTML file
+
+
+// function that hides the color option select menu until design option is chosen
+// and only displays T-shirt color options to matched design options
+
+
+// function that disables or activates conflicting scheduled event checkboxes
+// as they are matched
+
+
+// function that displays a running total of checked events.
+
+
+// function that only displays corresponding payment method
+// CC is first by default
---
+++ js/validation.js
@@ -0,0 +1,17 @@
+/* Form Validation JS file to choose HTML elements and show
+error messages/css styles both at at time of user input and at submit button
+*/
+
+// Selecting HTML elements to validate and storing in Variables
+
+// name field function
+
+// email field function
+
+// checkbox field function
+
+// credit card number field function
+
+// credit card date function
+
+// credit card CVV function
