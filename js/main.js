ebc50fad999be7cbe081a779611e211876838948
MASTER-PNZ <paulkeesling@gmail.com>
2018-04-12 17:21:06 -0400

    deleted shirt function
    added $ for jqueary variables

--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
-// Variables to get html form elements
+// Variables to get html elements
 
-const nameField = $('#name');
-const otherJob = $('#other-title');
-const selectMenu = $('#title');
-//let selectOpt = $('#title option:selected').text();
+const $nameField = $('#name');
+const $otherJob = $('#other-title');
+const $jobSelectMenu = $('#title');
+
+const $colorSelectmenu = $('#color');
+const $themeSelectmenu = $('#design');
+
 // Setting focus to first form field on pageLoad
 
-nameField.focus();
+$nameField.focus();
 
 // Hide the Other Job Role Html option until user clicks other
 // add user input to a text field that already exists in HTML file
-otherJob.hide();
-selectMenu.change(function(){
-  let selectOpt = $('#title option:selected').text();
-    if(selectOpt === "Other") {
-      otherJob.show();
+$otherJob.hide();
+$jobSelectMenu.change(function(){
+  let $selectOpt = $('#title option:selected').text();
+    if($selectOpt == "Other") {
+      $otherJob.show();
     } else {
-      otherJob.hide();
+      $otherJob.hide();
     }
 });
 
+
 // function that hides the color option select menu until design option is chosen
 // and only displays T-shirt color options to matched design options
+
 
 
 // function that disables or activates conflicting scheduled event checkboxes
