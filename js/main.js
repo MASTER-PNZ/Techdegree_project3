ba53fd9871c4d0cb5cde3f3fd1f79d756d235a41
MASTER-PNZ <paulkeesling@gmail.com>
2018-04-13 19:32:36 -0400

    fixed functions to remove and add color menus

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
 // Variables to get html elements
-
 const $nameField = $('#name');
 const $otherJob = $('#other-title');
 const $jobSelectMenu = $('#title');
-
 const $colorSelectMenu = $('#color');
 const $themeSelectMenu = $('#design');
-
 // Setting focus to first form field on pageLoad
-
 $nameField.focus();
-
 // Hide the Other Job Role Html option until user clicks other
 // add user input to a text field that already exists in HTML file
 $otherJob.hide();
@@ -22,30 +17,39 @@ $jobSelectMenu.change(function(){
       $otherJob.hide();
     }
 });
-
-
 // function that hides the color option select menu until design option is chosen
 // and only displays T-shirt color options to matched design options
-
+function colorOptionNone () {
+  $('#color option').each(function(index){
+        $(this).remove();
+      });
+}
 function colorOption1 () {
-    $('#color option').each(function(index){
-        if(index <= 2) {
-          $(this).append();
-        } else {
-          $(this).remove();
-        }
-    });
+  $colorSelectMenu.append('<option value="cornflowerblue">Cornflower Blue</option>');
+  $colorSelectMenu.append('<option value="darkslategrey">Dark Slate Grey</option>');
+  $colorSelectMenu.append('<option value="gold">Gold</option>');
 }
-
 function colorOption2 () {
-    $('#color option').each(function(index){
-        if(index >= 3) {
-          $(this).append();
-        } else {
-            $(this).remove();
-        }
-    });
+  $colorSelectMenu.append('<option value="tomato">Tomato</option>');
+  $colorSelectMenu.append('<option value="steelblue">Steel Blue</option>');
+  $colorSelectMenu.append('<option value="dimgrey">Dim Grey</option>');
 }
+$('#colors-js-puns').hide();
+$themeSelectMenu.change(function(){
+  colorOptionNone();
+   $('#colors-js-puns').show(1000);
+  let $selectedTheme = $('#design option:selected').text();
+  const $punsThemes = $('#design option[value="js puns"]').text();
+  const $heartThemes = $('#design option[value="heart js"]').text();
+  const $selectTheme = $('#design option[value="Select Theme"]').text();
+  if ($selectedTheme == $punsThemes) {
+    colorOption1();
+  } else if ($selectedTheme == $heartThemes) {
+    colorOption2();
+  } else {
+     $('#colors-js-puns').hide(1000);
+  }
+});
 
 
 // function that disables or activates conflicting scheduled event checkboxes
